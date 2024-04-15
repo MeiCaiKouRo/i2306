195303f8b90ee09506330957e6218b27fd7879be
adrian <13992006455@163.com>
2024-04-15 15:11:59 +0800

    init order&seat DB config

--- common/src/main/java/com/next/db/DataSources.java
+++ common/src/main/java/com/next/db/DataSources.java
@@ -8,15 +8,15 @@ public interface DataSources {
 
     String TRAIN_ORDER_DB = "trainOrderDB";
 
-    String TRAIN_ORDER_DB_1 = "trainOrderDB1";
+    String TRAIN_SEAT_DB_1 = "trainSeatDB1";
 
-    String TRAIN_ORDER_DB_2 = "trainOrderDB2";
+    String TRAIN_SEAT_DB_2 = "trainSeatDB2";
 
-    String TRAIN_ORDER_DB_3 = "trainOrderDB3";
+    String TRAIN_SEAT_DB_3 = "trainSeatDB3";
 
-    String TRAIN_ORDER_DB_4 = "trainOrderDB4";
+    String TRAIN_SEAT_DB_4 = "trainSeatDB4";
 
-    String TRAIN_ORDER_DB_5 = "trainOrderDB5";
+    String TRAIN_SEAT_DB_5 = "trainSeatDB5";
 
 
 }
--- common/src/main/java/com/next/db/OrderDataSourceConfig.java
+++ common/src/main/java/com/next/db/OrderDataSourceConfig.java
@@ -1,11 +1,47 @@
 package com.next.db;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
-@MapperScan(basePackages = "com.next.orderDao", sqlSessionTemplateRef = "")
+@MapperScan(basePackages = "com.next.orderDao", sqlSessionTemplateRef = "orderSqlSessionTemplate")
 public class OrderDataSourceConfig {
+
+    @ConfigurationProperties(prefix = "spring.datasource-order")
+    @Bean(name = DataSources.TRAIN_ORDER_DB)
+    public DataSource orderDB(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "orderTransactionManager")
+    public DataSourceTransactionManager orderTransactionManager(@Qualifier(DataSources.TRAIN_ORDER_DB)DataSource orderDB){
+        return new DataSourceTransactionManager(orderDB);
+    }
+
+    @Bean(name = "orderSqlSessionFactory")
+    public SqlSessionFactory orderSqlSessionFactory(@Qualifier(DataSources.TRAIN_ORDER_DB) DataSource orderDB) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(orderDB);
+        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:orderMappers/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean(name = "orderSqlSessionTemplate")
+    public SqlSessionTemplate orderSqlSessionTemplate(@Qualifier("orderSqlSessionFactory") SqlSessionFactory orderSqlSessionFactory) {
+        return  new SqlSessionTemplate(orderSqlSessionFactory);
+    }
+
 
 
 }
